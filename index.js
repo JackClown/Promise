a3213e8a9a56421bc080b07bbde54a3d1b48a909
JackClown <309342593@qq.com>
2016-05-18 23:16:25 +0800

    renamed:    Promise.js -> index.js 	renamed:    index.js -> test.js

--- index.js
+++ index.js
@@ -1,6 +1,144 @@
-var adapter = require('./adapter');
-var promisesAplusTests = require("promises-aplus-tests");
+function emit(type, elm) {
+  if (Array.isArray(elm.evnQue[type])) {
+    for (var i = 0; i < elm.evnQue[type].length; i++) {
+      elm.evnQue[type][i]();
+    }
+  }
+}
 
-promisesAplusTests(adapter, function (err) {
-	console.log(err);
-});
+function bind(elm, type, handle) {
+  if (!elm.evnQue[type]) elm.evnQue[type] = [];
+  elm.evnQue[type].push(handle);
+}
+
+function Promise(excutor) {
+  this.pm = {
+    status: 'pending',
+    value: '',
+    reason: ''
+  };
+  this.evnQue = {};
+
+  var self = this;
+
+  if(typeof excutor != 'function') throw new TypeError('Promise excutor is not Function');
+
+  excutor(function(value) {
+    self.pm.value = value;
+    self.pm.status = 'fulfilled';
+    emit('resolve', self);
+  }, function(reason) {
+    self.pm.reason = reason;
+    self.pm.status = 'rejected';
+    emit('reject', self);
+  });
+}
+
+Promise.resolve = function(pro, x) {
+  if(pro.pm.status == 'rejected') return;
+
+  if (x && x.constructor == Promise) {
+    if(x.pm.status == 'fulfilled') {
+      Promise.resolve(pro, x.pm.value);
+      return;
+    }else if(x.pm.status == 'rejected'){
+      Promise.reject(pro, x.pm.reason);
+      return;
+    }else {
+      pro.pm = x.pm;
+      x.evnQue = pro.evnQue;
+    }
+  } else if (x && (typeof x == 'object' || typeof x == 'function') && ('then' in x)) {
+    try {
+      var then = x.then;
+    }catch(e){
+      Promise.reject(pro, e);
+      return;
+    }
+
+    if(typeof then != 'function'){
+      pro.pm.value = x;
+      pro.pm.status = 'fulfilled';
+    }else {
+      var times = 0;
+      try {
+        then.call(x, function(value) {
+          if(times > 0) return;
+          Promise.resolve(pro, value);
+          times++;
+        }, function(reason) {
+          if(times > 0) return;
+          Promise.reject(pro, reason);
+          times++;
+        })
+      } catch(e){
+        if(times === 0) Promise.reject(pro, e);
+        times++;
+      }
+
+      return;
+    }
+  } else {
+    pro.pm.value = x;
+    pro.pm.status = 'fulfilled';
+  }
+
+  if(pro.pm.status == 'fulfilled') emit('resolve', pro);
+  else if(pro.pm.status) emit('reject', pro);
+};
+
+Promise.reject = function(pro, e) {
+  if(pro.pm.status == 'fulfilled') return;
+
+  pro.pm.status = 'rejected';
+  pro.pm.reason = e;
+
+  emit('reject', pro);
+};
+
+Promise.prototype.then = function(onRes, onRej) {
+  var next = new Promise(function(res, rej) {});
+  var self = this;
+
+  if(self.pm.status == 'pending'){
+    bind(self, 'resolve', handle);
+    bind(self, 'reject', handle);
+  }else {
+    handle();
+  }
+
+  return next;
+
+  function handle() {
+    setTimeout(function(){
+      try {
+        var result = null;
+
+        if (self.pm.status == 'fulfilled') {
+          if(typeof onRes != 'function'){
+            Promise.resolve(next, self.pm.value);
+            return;
+          }
+
+          result = onRes(self.pm.value);
+         }
+        else if (self.pm.status == 'rejected') {
+          if(typeof onRej != 'function'){
+            Promise.reject(next, self.pm.reason);
+            return;
+          }
+
+          result = onRej(self.pm.reason);
+        }
+
+        if(result == next){
+          Promise.reject(next, new TypeError());
+        }else Promise.resolve(next, result);
+      } catch (e) {
+        Promise.reject(next, e);
+      }
+    });
+  }
+};
+
+module.exports = Promise;
--- test.js
+++ test.js
@@ -1,35 +1,6 @@
-var assert = require("assert");
-
 var adapter = require('./adapter');
-var resolved = adapter.resolved || function(value){
-	return new Promise(function(res){
-		res(value);
-	});
-};
-var rejected = adapter.rejected ||function(reason){
-	return new Promise(function(res, rej){
-		rej(reason);
-	});
-};
-var deferred = adapter.deferred;
-var dummy = {
-	dummy: 'dummy'
-};
+var promisesAplusTests = require("promises-aplus-tests");
 
-var bummy = {
-	bummy: 'bummy'
-};
-
-var done = function() {
-	console.log('done');
-};
-
-var promise = resolved(dummy);
-var expectedValue = undefined;
-
-promise.then(function onFulfilled() {
-	return expectedValue;
-}).then(function onPromise2Fulfilled(actualValue) {
-	assert.strictEqual(actualValue, expectedValue);
-	done();
-})
+promisesAplusTests(adapter, function (err) {
+	console.log(err);
+});
