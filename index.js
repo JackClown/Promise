287cad2dc02fa7fdddb027d905e82b95ffa8faf9
JackClown <309342593@qq.com>
2016-05-22 10:22:51 +0800

    modified:   index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function Promise(excutor) {
 
   var self = this;
 
-  if(typeof excutor != 'function') throw new TypeError('Promise excutor is not Function');
+  if (typeof excutor != 'function') throw new TypeError('Promise excutor is not Function');
 
   excutor(function(value) {
     self.pm.value = value;
@@ -35,46 +35,46 @@ function Promise(excutor) {
 }
 
 Promise.resolve = function(pro, x) {
-  if(pro.pm.status == 'rejected') return;
+  if (pro.pm.status == 'rejected') return;
 
   if (x && x.constructor == Promise) {
-    if(x.pm.status == 'fulfilled') {
+    if (x.pm.status == 'fulfilled') {
       Promise.resolve(pro, x.pm.value);
       return;
-    }else if(x.pm.status == 'rejected'){
+    } else if (x.pm.status == 'rejected') {
       Promise.reject(pro, x.pm.reason);
       return;
-    }else {
+    } else {
       pro.pm = x.pm;
       x.evnQue = pro.evnQue;
     }
   } else if (x && (typeof x == 'object' || typeof x == 'function') && ('then' in x)) {
     var then = null;
-    
+
     try {
       then = x.then;
-    }catch(e){
+    } catch (e) {
       Promise.reject(pro, e);
       return;
     }
 
-    if(typeof then != 'function'){
+    if (typeof then != 'function') {
       pro.pm.value = x;
       pro.pm.status = 'fulfilled';
-    }else {
+    } else {
       var times = 0;
       try {
         then.call(x, function(value) {
-          if(times > 0) return;
+          if (times > 0) return;
           Promise.resolve(pro, value);
           times++;
         }, function(reason) {
-          if(times > 0) return;
+          if (times > 0) return;
           Promise.reject(pro, reason);
           times++;
         });
-      } catch(e){
-        if(times === 0) Promise.reject(pro, e);
+      } catch (e) {
+        if (times === 0) Promise.reject(pro, e);
         times++;
       }
 
@@ -85,12 +85,12 @@ Promise.resolve = function(pro, x) {
     pro.pm.status = 'fulfilled';
   }
 
-  if(pro.pm.status == 'fulfilled') emit('resolve', pro);
-  else if(pro.pm.status == 'rejected') emit('reject', pro);
+  if (pro.pm.status == 'fulfilled') emit('resolve', pro);
+  else if (pro.pm.status == 'rejected') emit('reject', pro);
 };
 
 Promise.reject = function(pro, e) {
-  if(pro.pm.status == 'fulfilled') return;
+  if (pro.pm.status == 'fulfilled') return;
 
   pro.pm.status = 'rejected';
   pro.pm.reason = e;
@@ -102,30 +102,29 @@ Promise.prototype.then = function(onRes, onRej) {
   var next = new Promise(function(res, rej) {});
   var self = this;
 
-  if(self.pm.status == 'pending'){
+  if (self.pm.status == 'pending') {
     bind(self, 'resolve', handle);
     bind(self, 'reject', handle);
-  }else {
+  } else {
     handle();
   }
 
   return next;
 
   function handle() {
-    setTimeout(function(){
+    setTimeout(function() {
       try {
         var result = null;
 
         if (self.pm.status == 'fulfilled') {
-          if(typeof onRes != 'function'){
+          if (typeof onRes != 'function') {
             Promise.resolve(next, self.pm.value);
             return;
           }
 
           result = onRes(self.pm.value);
-         }
-        else if (self.pm.status == 'rejected') {
-          if(typeof onRej != 'function'){
+        } else if (self.pm.status == 'rejected') {
+          if (typeof onRej != 'function') {
             Promise.reject(next, self.pm.reason);
             return;
           }
@@ -133,9 +132,10 @@ Promise.prototype.then = function(onRes, onRej) {
           result = onRej(self.pm.reason);
         }
 
-        if(result == next){
+        if (result == next)
           Promise.reject(next, new TypeError());
-        }else Promise.resolve(next, result);
+        else
+          Promise.resolve(next, result);
       } catch (e) {
         Promise.reject(next, e);
       }
